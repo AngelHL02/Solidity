f1a268af7f494f26a4429a0deb1e7aacd8e2453b
AngelHL02 <hei_laam@yahoo.com.hk>
2023-03-09 13:08:00 +0800

    9Mar(13:07)

--- contracts/Project Structure.sol
+++ contracts/Project Structure.sol
@@ -1,3 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract verification{
+
+    //variables to be defined
+    address public manager;
+
+    constructor() public {
+        manager = msg.sender;
+    }
+
+    struct patient{
+        string name;
+        uint hkid;
+        uint timestamp; 
+    }
+
+
+
+
+}
